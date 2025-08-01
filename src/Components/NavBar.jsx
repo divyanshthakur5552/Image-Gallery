0105bd9fafc6e0d021c042c60363626fdba7cbbd
divyanshthakur5552 <imnotdivyansh@gmail.com>
2025-08-02 04:39:58 +0530

    Your commit message

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,9 @@
 import { BsImageAlt } from "react-icons/bs";
 import { IoSearchCircleOutline } from "react-icons/io5";
 import { FaRegSun } from "react-icons/fa6";
+import { HiMenuAlt3, HiX } from "react-icons/hi";
 import { NavLink, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import "./NavBar.css";
 
@@ -13,41 +15,101 @@ export default function Navbar({
   setInputValue,
 }) {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = isMenuOpen ? "hidden" : "auto";
+  }, [isMenuOpen]);
 
   return (
-    <div className="navbar">
-      <div className="logo" onClick={() => navigate("/")}>
-        <BsImageAlt className="logo-icon" />
-        <h1 className="logo-text">Image Gallery</h1>
+    <>
+      <div className="navbar">
+        <div className="navbar-main">
+          <div className="logo" onClick={() => navigate("/")}>
+            <BsImageAlt className="logo-icon" />
+            <h1 className="logo-text">Image Gallery</h1>
+          </div>
+
+          <div className={`search-box ${isSearchOpen ? "search-open" : ""}`}>
+            <input
+              type="text"
+              value={inputValue}
+              placeholder="Search"
+              onKeyPress={handleKeyPress}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <IoSearchCircleOutline
+              onClick={handleSearch}
+              className="search-icon"
+            />
+          </div>
+
+          <div className="navbar-right">
+            <ul className={`nav-menu ${isMenuOpen ? "nav-menu-open" : ""}`}>
+              <li>
+                <NavLink to="/" end onClick={closeMenu}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/saved" onClick={closeMenu}>
+                  Saved
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" onClick={closeMenu}>
+                  About
+                </NavLink>
+              </li>
+            </ul>
+
+            <div className="icon-group">
+              <IoSearchCircleOutline
+                className="mobile-search-icon"
+                onClick={toggleSearch}
+              />
+              <FaRegSun className="toggle-icon" onClick={toggleTheme} />
+              <div className="hamburger" onClick={toggleMenu}>
+                {isMenuOpen ? <HiX /> : <HiMenuAlt3 />}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="search-box">
-        <input
-          type="text"
-          value={inputValue}
-          placeholder="Search"
-          onKeyPress={handleKeyPress}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <IoSearchCircleOutline onClick={handleSearch} className="search-icon" />
+      {/* moved outside navbar to prevent clipping */}
+      <div
+        className={`mobile-search-overlay ${
+          isSearchOpen ? "search-overlay-open" : ""
+        }`}
+      >
+        <div className="mobile-search-box">
+          <input
+            type="text"
+            value={inputValue}
+            placeholder="Search"
+            onKeyPress={handleKeyPress}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <IoSearchCircleOutline
+            onClick={handleSearch}
+            className="search-icon"
+          />
+        </div>
       </div>
-
-      <div className="navbar-right">
-        <ul>
-          <li>
-            <NavLink to="/" end>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/saved">Saved</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-        </ul>
-        <FaRegSun className="toggle-icon" onClick={toggleTheme} />
-      </div>
-    </div>
+    </>
   );
 }
